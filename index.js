7b68c2971dc8eac753aefd62a2ad1a4564a54400
Tej <tejaa@menlo-technologies.com>
2019-07-19 16:26:26 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const process = require('process');
+
+app.get('/', (req, res) =>
+    res.send('Hello wo!'))
+
+app.get('/api', (req, res) => {
+    console.log('========');
+    res.send('===========')
+})
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
